4dd2564e81bf93218a62824512ecabd8fafec4c0
MSK619 <msk84067@gmail.com>
2024-11-27 03:03:43 +0530

    branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tiles-game';
+export class AppComponent implements OnInit {
+
+  rows = 4;
+  cols = 4;
+
+  boxes = [];
+
+  boxesId = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.populateData();
+  }
+
+  populateData() {
+
+    for (let r = 0; r < this.rows; r++) {
+      console.log(r);
+      console.log(r);
+      console.log(r);
+    }
+    console.log('fdsfs');
+  }
 }
